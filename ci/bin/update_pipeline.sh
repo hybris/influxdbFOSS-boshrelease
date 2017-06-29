8f5b9d0bebf37550dec9ca75196c369d90f1efe8
Marek Nieznalski <marek.nieznalski@gmail.com>
2017-06-29 13:56:02 +0200

    Prepare for concourse upgrade - OD-2738

--- ci/bin/update_pipeline.sh
+++ ci/bin/update_pipeline.sh
@@ -7,7 +7,7 @@ PIPELINE_NAME=${PIPELINE_NAME:-"influxdbFOSS-boshrelease"}
 
 CREDENTIALS=$(mktemp /tmp/credentials.XXXXX)
 
-vault read -field=value -tls-skip-verify secret/concourse/$PIPELINE_NAME > ${CREDENTIALS}
+vault read -format=json -tls-skip-verify secret/concourse/$PIPELINE_NAME | jq '.data' > ${CREDENTIALS}
 
 fly -t ${TARGET} set-pipeline -c pipeline.yml --load-vars-from=${CREDENTIALS} --pipeline=${PIPELINE_NAME}
 if [ $? -ne 0 ]; then
