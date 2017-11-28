dd90e3d554ed69cf14a0ce497d9d7dc10ca75316
mwieczorek <wieczorek-michal@wp.pl>
2017-11-28 16:33:03 +0100

    Changed pipelines to enable native concourse-vault integration

--- ci/bin/update_pipeline.sh
+++ ci/bin/update_pipeline.sh
@@ -5,13 +5,7 @@ set -e
 TARGET=${TARGET:-"hybris"}
 PIPELINE_NAME=${PIPELINE_NAME:-"influxdbFOSS-boshrelease"}
 
-CREDENTIALS=$(mktemp /tmp/credentials.XXXXX)
-
-vault read -format=json -tls-skip-verify secret/concourse/$PIPELINE_NAME | jq '.data' > ${CREDENTIALS}
-
-fly -t ${TARGET} set-pipeline -c pipeline.yml --load-vars-from=${CREDENTIALS} --pipeline=${PIPELINE_NAME}
+fly -t ${TARGET} set-pipeline -c pipeline.yml --pipeline=${PIPELINE_NAME}
 if [ $? -ne 0 ]; then
   echo "Please login first: fly -t ${TARGET} login -k"
 fi
-
-rm -f ${CREDENTIALS}
